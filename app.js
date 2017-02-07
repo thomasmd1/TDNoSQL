95fea16926408c6464668b9dc38c227b8efa51ab
Benjamin Thomas <benjaminthomas@MacBook-Pro-de-Benjamin.local>
2017-02-07 13:56:14 +0100

    update jsonParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Permet de changer le système de promesses de mongo par celui de Node
 mongoose.Promise = global.Promise;
