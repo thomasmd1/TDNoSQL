9e7ed9e511c14e32d83a847ccdf95fb55774392b
Benjamin Thomas <benjaminthomas@pc69.home>
2017-02-07 20:30:26 +0100

    affichage personne + pagination fini

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,13 +3,48 @@ var fs = require('fs');
 var Person = require('../models/Person');
 
 // Permet de créer une route qui map l'url "/" en GET
-router.get('/', function(req, res) {
-    // Permet de retrouver des résultats sur un modèle
+router.get('/page/:page', function(req, res) {
+  if(!req.params.page){
+    next()
+  }
     Person.find({}).then(function(persons) {
-        // Permet d'afficher une vue et de lui passer des paramètres
-        res.render('home.ejs', { persons: persons});
-    });
+      var currentPage=parseInt(req.params.page);
+      var nbPage = persons.length/100;
+      var min,limit,pageMin,pageMax;
 
+      pageMin = currentPage-5;
+      pageMax= currentPage+5;
+
+      if(currentPage<=0){
+        currentPage=1;
+      }
+      min=currentPage*100-100;
+      limit=100;
+
+      Person.find({}).skip(min).limit(limit).then(function(p) {
+          // Permet d'afficher une vue et de lui passer des paramètre
+          res.render('home.ejs', { persons: p, pages: nbPage, current: currentPage, pmi: pageMin, pma: pageMax});
+      });
+    });
+});
+
+router.get('/', function(req, res) {
+  Person.find({}).then(function(persons) {
+    var nbPage = persons.length/100;
+    var currentPage=parseInt(1);
+    var min,limit;
+
+    if(currentPage<=0){
+      currentPage=1;
+    }
+    min=currentPage*100-100;
+    limit=100;
+
+    Person.find({}).skip(min).limit(limit).then(function(p) {
+      // Permet d'afficher une vue et de lui passer des paramètre
+      res.render('home.ejs', { persons: p, pages: nbPage, current: currentPage});
+    });
+  });
 });
 
 // Permet de créer une route qui map l'url "/hello" en GET
